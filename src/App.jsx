c8754c16f986afe5a4c21a7b65bf3052a210729b
Carlito <carlorody1000@gmail.com>
2021-04-05 09:46:13 -0500

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+/* eslint no-eval: 0 */
+import React, { useState } from 'react'
 import Functions from './components/Functions'
 import Numbers from './components/Numbers'
 import MathOperations from './components/MathOperations'
@@ -6,26 +7,39 @@ import Result from './components/Result'
 import './App.css'
 
 const App = () => {
+    const [stack, setStack] = useState("");
+
     return (
     <main className='react-calculator'>
         <h1 className="title">Calculadora</h1>
-        <Result value={undefined} />
+        <Result value={stack} />
         <Numbers onClickNumber={number => {
             console.log("Click en number", number) 
+            setStack(`${stack}${number}`)
         }} />
         <Functions 
-            onContentClear={() => 
-                console.log("Content Clear")}
-            onDelete={() => 
-                console.log("onDelete")}
+            onContentClear={() => {
+                console.log("Content Clear")
+                setStack('')
+            }}
+            onDelete={() => {
+                if(stack.length > 0) {
+                    const newStack = stack.substring(0, stack.length-1);
+                    
+                    setStack(newStack);
+                }
+            }}
         />
         <MathOperations 
-            onClickOperation={operation => 
+            onClickOperation={operation => {
                 console.log("Operation:", operation)
-            } 
-            onClickEqual={equal => 
+                setStack(`${stack} ${operation}`) 
+            }} 
+            onClickEqual={equal => {
                 console.log("Igual:", equal)
-            }
+                // setStack(`${stack} ${equal}`) 
+                setStack(eval(stack).toString())
+            }}
         />
     </main>)
 }
